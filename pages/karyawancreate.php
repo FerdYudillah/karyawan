ecb8b804930aeeaa313ce6143bad76b207292208
Ferdy <akbarferdi59@gmail.com>
2022-05-14 12:42:10 +0800

    Redirect setelah Simpan

--- pages/karyawancreate.php
+++ pages/karyawancreate.php
@@ -24,6 +24,9 @@
                 $statement->bindParam(3, $jenis_kelamin);
                 $statement->bindParam(4, $status_menikah);
                 $statement->execute();
+
+                header('Location: main.php?page=karyawan');
+
               }
              
             
