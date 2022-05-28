67c6042cd61b4e4b9f4d93cd91cdbef377e364d2
Ferdy <akbarferdi59@gmail.com>
2022-05-28 20:15:28 +0800

    Menambahkan Routes

--- pages/routes.php
+++ pages/routes.php
@@ -19,7 +19,16 @@ if(isset($_GET['page'])){
             break;
         case 'bagian':
             include "bagian/bagian.php";
-            break;    
+            break;  
+         case 'bagiancreate':
+                include "bagian/bagiancreate.php";
+             break;   
+         case 'bagianupdate':
+             include "bagian/bagianupdate.php";
+             break;  
+         case 'bagiandelete':
+            include "bagian/bagiandelete.php";
+            break;             
         default:
             include "dashboard.php";
             break;
