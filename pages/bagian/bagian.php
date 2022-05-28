8bdbef3294fef4dba4b1eda2048ed49fc72a5939
Ferdy <akbarferdi59@gmail.com>
2022-05-28 20:16:13 +0800

    Membuat Halaman Bagian

--- pages/bagian/bagian.php
+++ pages/bagian/bagian.php
@@ -1,134 +1,55 @@
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Bagian</h1>
+    <h1 class="h2">Data Bagian</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
     </div>
   </div>
 
   <div class="table-responsive">
+  <a href="?page=bagiancreate" class="btn btn-success mb-3"> <span data-feather="users"></span> TAMBAH DATA</a>
     <table class="table table-striped table-sm">
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Header</th>
-          <th scope="col">Header</th>
-          <th scope="col">Header</th>
-          <th scope="col">Header</th>
+          <th scope="col">No</th>
+          <th scope="col">NIK</th>
+          <th scope="col">Nama Karyawan</th>
+          <th scope="col">Jenis Kelamin</th>
+          <th scope="col">Bagian</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1,001</td>
-          <td>random</td>
-          <td>data</td>
-          <td>placeholder</td>
-          <td>text</td>
-        </tr>
-        <tr>
-          <td>1,002</td>
-          <td>placeholder</td>
-          <td>irrelevant</td>
-          <td>visual</td>
-          <td>layout</td>
-        </tr>
-        <tr>
-          <td>1,003</td>
-          <td>data</td>
-          <td>rich</td>
-          <td>dashboard</td>
-          <td>tabular</td>
-        </tr>
-        <tr>
-          <td>1,003</td>
-          <td>information</td>
-          <td>placeholder</td>
-          <td>illustrative</td>
-          <td>data</td>
-        </tr>
-        <tr>
-          <td>1,004</td>
-          <td>text</td>
-          <td>random</td>
-          <td>layout</td>
-          <td>dashboard</td>
-        </tr>
-        <tr>
-          <td>1,005</td>
-          <td>dashboard</td>
-          <td>irrelevant</td>
-          <td>text</td>
-          <td>placeholder</td>
-        </tr>
-        <tr>
-          <td>1,006</td>
-          <td>dashboard</td>
-          <td>illustrative</td>
-          <td>rich</td>
-          <td>data</td>
-        </tr>
-        <tr>
-          <td>1,007</td>
-          <td>placeholder</td>
-          <td>tabular</td>
-          <td>information</td>
-          <td>irrelevant</td>
-        </tr>
-        <tr>
-          <td>1,008</td>
-          <td>random</td>
-          <td>data</td>
-          <td>placeholder</td>
-          <td>text</td>
-        </tr>
-        <tr>
-          <td>1,009</td>
-          <td>placeholder</td>
-          <td>irrelevant</td>
-          <td>visual</td>
-          <td>layout</td>
-        </tr>
-        <tr>
-          <td>1,010</td>
-          <td>data</td>
-          <td>rich</td>
-          <td>dashboard</td>
-          <td>tabular</td>
-        </tr>
-        <tr>
-          <td>1,011</td>
-          <td>information</td>
-          <td>placeholder</td>
-          <td>illustrative</td>
-          <td>data</td>
-        </tr>
-        <tr>
-          <td>1,012</td>
-          <td>text</td>
-          <td>placeholder</td>
-          <td>layout</td>
-          <td>dashboard</td>
-        </tr>
-        <tr>
-          <td>1,013</td>
-          <td>dashboard</td>
-          <td>irrelevant</td>
-          <td>text</td>
-          <td>visual</td>
-        </tr>
-        <tr>
-          <td>1,014</td>
-          <td>dashboard</td>
-          <td>illustrative</td>
-          <td>rich</td>
-          <td>data</td>
-        </tr>
-        <tr>
-          <td>1,015</td>
-          <td>random</td>
-          <td>tabular</td>
-          <td>information</td>
-          <td>text</td>
-        </tr>
+      <?php 
+              $selectSQL = "SELECT * FROM bagian";
+              $database = new Database();
+              $connection = $database->getConnection();
+              $statement = $connection->prepare( $selectSQL);
+              $statement->execute();  
+
+              $no = 1;
+              while ($data = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+              ?>
+              <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $data['nik'] ?></td>
+                <td><?php echo $data['nama'] ?></td>
+                <td><?php echo $data['jenis_kelamin'] ?></td>
+                <td><?php echo $data['bagian'] ?></td>
+                <td>
+                  
+                <a href="?page=bagianupdate&id=<?php echo $data['id'] ?>" class="badge bg-warning">
+                <span data-feather="edit">
+              </a>
+              <a href="?page=bagiandelete&id=<?php echo $data['id'] ?>" class="badge bg-danger">
+                <span data-feather="x-octagon">
+              </a>
+                </td>
+                
+              </tr>
+              <?php 
+              }
+              ?>
+            </tbody>
       </tbody>
     </table>
   </div>
